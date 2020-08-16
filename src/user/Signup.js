d4fb61b4a1a638c47610b9eb064731a91a968461
arunsnd2017 <arunsinghsnd@gmail.com>
2020-08-16 22:35:24 +0530

    working on signup routes

---
+++ src/user/Signup.js
@@ -0,0 +1,15 @@
+import React , {useState} from 'react'
+import Base from '../core/Base'
+import {Link} from 'react-router-dom'
+
+
+const Signup = () =>{
+    return(
+        <Base title="Sign Up page" description="A page for user to sign up!">
+            <h1>Signup works</h1>
+        </Base>
+    )
+}
+
+
+export default Signup;
