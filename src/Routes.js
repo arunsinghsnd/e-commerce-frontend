b5b8a5efc20b7e4ea79372774e1dbb51100d4b41
arunsnd2017 <arunsinghsnd@gmail.com>
2020-08-20 20:54:06 +0530

    creating add category in addmin panle

--- src/Routes.js
+++ src/Routes.js
@@ -7,6 +7,8 @@ import AdminRoute from "./auth/helper/AdminRoutes";
 import PrivateRoute from "./auth/helper/PrivateRoutes";
 import UserDashBoard from "./user/UserDashBoard";
 import AdminDashBoard from "./user/AdminDashBoard";
+import AddCategory from "./admin/AddCategory";
+
 
 const Routes = () => {
 return (
@@ -17,6 +19,7 @@ return (
         <Route path="/signin" exact component={Signin} />
         <PrivateRoute path="/user/dashboard" exact component={UserDashBoard} />
         <AdminRoute path="/admin/dashboard" exact component={AdminDashBoard} />
+        <AdminRoute path="/admin/create/category" exact component={AddCategory} />
     </Switch>
     </BrowserRouter>
 );
---
+++ src/admin/AddCategory.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Base from '../core/Base';
+
+const  AddCategory = () => {
+    return (
+        <div>
+            <h1>Create a new category from here</h1>
+        </div>
+    );
+};
+
+export default AddCategory;
