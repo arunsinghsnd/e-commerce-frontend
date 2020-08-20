c7f15bdb61d85759ec1d234c9127b742bca31e8e
arunsnd2017 <arunsinghsnd@gmail.com>
2020-08-20 20:42:38 +0530

    complting the admin plane in right side

--- src/user/AdminDashBoard.js
+++ src/user/AdminDashBoard.js
@@ -4,44 +4,56 @@ import { isAutheticated } from "../auth/helper/index";
 import { Link } from "react-router-dom";
 
 const AdminDashBoard = () => {
-const {
+  const {
     user: { name, email, role }
-} = isAutheticated();
+  } = isAutheticated();
 
-const adminLeftSide = () => {
+  const adminLeftSide = () => {
     return (
-    <div className="card">
+      <div className="card">
         <h4 className="card-header bg-dark text-white">Admin Navigation</h4>
         <ul className="list-group">
-        <li className="list-group-item">
+          <li className="list-group-item">
             <Link to="/admin/create/category" className="nav-link text-success">
-            Create Categories
+              Create Categories
             </Link>
-        </li>
-        <li className="list-group-item">
+          </li>
+          <li className="list-group-item">
             <Link to="/admin/create/product" className="nav-link text-success">
-            Create Product
+              Create Product
             </Link>
-        </li>
-        <li className="list-group-item">
+          </li>
+          <li className="list-group-item">
             <Link to="/admin/products" className="nav-link text-success">
-            Manage Products
+              Manage Products
             </Link>
-        </li>
-        <li className="list-group-item">
+          </li>
+          <li className="list-group-item">
             <Link to="/admin/orders" className="nav-link text-success">
-            Manage Orders
+              Manage Orders
             </Link>
-        </li>
+          </li>
         </ul>
-    </div>
+      </div>
     );
-};
+  };
 
   const adminRightSide = () => {
     return (
-      <div>
-        <h1>Hi admin</h1>
+      <div className="card mb-4">
+        <h4 className="card-header">Admin Information</h4>
+        <ul className="list-group">
+          <li className="list-group-item">
+            <span className="badge badge-success mr-2">Name:</span> {name}
+          </li>
+          <li className="list-group-item">
+            <span className="badge badge-success mr-2">Email:</span> {email}
+          </li>
+
+          <li className="list-group-item">
+            <span className="badge badge-danger">Admin Area</span>
+          </li>
+        </ul>
       </div>
     );
   };
