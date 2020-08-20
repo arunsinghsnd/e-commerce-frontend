b56f7fbbb76c52c2d1368355b25d9d3e1b08645d
arunsnd2017 <arunsinghsnd@gmail.com>
2020-08-20 20:37:23 +0530

    fixing the issue

--- src/user/AdminDashBoard.js
+++ src/user/AdminDashBoard.js
@@ -4,39 +4,39 @@ import { isAutheticated } from "../auth/helper/index";
 import { Link } from "react-router-dom";
 
 const AdminDashBoard = () => {
-  const {
+const {
     user: { name, email, role }
-  } = isAutheticated();
+} = isAutheticated();
 
-  const adminLeftSide = () => {
+const adminLeftSide = () => {
     return (
-      <div className="card">
+    <div className="card">
         <h4 className="card-header bg-dark text-white">Admin Navigation</h4>
         <ul className="list-group">
-          <li className="list-group-item">
+        <li className="list-group-item">
             <Link to="/admin/create/category" className="nav-link text-success">
-              Create Categories
+            Create Categories
             </Link>
-          </li>
-          <li className="list-group-item">
+        </li>
+        <li className="list-group-item">
             <Link to="/admin/create/product" className="nav-link text-success">
-              Create Product
+            Create Product
             </Link>
-          </li>
-          <li className="list-group-item">
+        </li>
+        <li className="list-group-item">
             <Link to="/admin/products" className="nav-link text-success">
-              Manage Products
+            Manage Products
             </Link>
-          </li>
-          <li className="list-group-item">
+        </li>
+        <li className="list-group-item">
             <Link to="/admin/orders" className="nav-link text-success">
-              Manage Orders
+            Manage Orders
             </Link>
-          </li>
+        </li>
         </ul>
-      </div>
+    </div>
     );
-  };
+};
 
   const adminRightSide = () => {
     return (
