7e305bcca80824e28f7bd44eca979ef4f946962e
arunsnd2017 <arunsinghsnd@gmail.com>
2020-08-21 11:16:41 +0530

    fetching the data to addproduct compoent

--- src/Routes.js
+++ src/Routes.js
@@ -9,6 +9,8 @@ import UserDashBoard from "./user/UserDashBoard";
 import AdminDashBoard from "./user/AdminDashBoard";
 import AddCategory from "./admin/AddCategory";
 import ManageCategories from "./admin/ManageCategories";
+import AddProduct from "./admin/AddProduct";
+
 
 
 
@@ -23,6 +25,8 @@ return (
         <AdminRoute path="/admin/dashboard" exact component={AdminDashBoard} />
         <AdminRoute path="/admin/create/category" exact component={AddCategory} />
         <AdminRoute path="/admin/categories" exact component={ManageCategories} />
+        <AdminRoute path="/admin/create/product" exact component={AddProduct} />
+
     </Switch>
     </BrowserRouter>
 );
