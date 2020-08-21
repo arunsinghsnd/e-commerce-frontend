9102cfc0ac2e7c06836fe4c7cf20a4bac68ccaa9
arunsnd2017 <arunsinghsnd@gmail.com>
2020-08-21 10:26:14 +0530

    update a product

--- src/admin/helper/adminapicall.js
+++ src/admin/helper/adminapicall.js
@@ -92,3 +92,20 @@ export const getProduct = productId => {
       .catch(err => console.log(err));
   };
   
+
+  //update a product
+
+export const updateProduct = (productId, userId, token, product) => {
+    return fetch(`${API}/product/${productId}/${userId}`, {
+      method: "PUT",
+      headers: {
+        Accept: "application/json",
+        Authorization: `Bearer ${token}`
+      },
+      body: product
+    })
+      .then(response => {
+        return response.json();
+      })
+      .catch(err => console.log(err));
+  };
