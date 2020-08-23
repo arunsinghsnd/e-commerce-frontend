08285b3f81217dc6acb20aaee82f841e979b663f
arunsnd2017 <arunsinghsnd@gmail.com>
2020-08-23 10:09:22 +0530

    adding card and fixing the bugs

--- src/core/Home.js
+++ src/core/Home.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import '../styles.css';
-import {API} from '../backend';
-import Base from './Base';
-
+import React from "react";
+import "../styles.css";
+import { API } from "../backend";
+import Base from "./Base";
+import Card from "./Card";
 
 export default function Home() {
-    console.log("API IS", API);
-    return (
-        <Base title="Home Page" description="Welcome to the Tshirt Store">
-            <div className="row">
-                <div className="col-4">
-                    <button className="btn btn-success">Test</button>
-                </div>
-                <div className="col-4">
-                    <button className="btn btn-success">Test</button>
-                </div>
-                <div className="col-4">
-                    <button className="btn btn-success">Test</button>
-                </div>
-            </div>
-        </Base>
-    );
+  return (
+    <Base title="Home Page" description="Welcome to the Tshirt Store">
+      <div className="row text-center">
+        <div className="col-4">
+          <Card />
+        </div>
+        <div className="col-4">
+          <button className="btn btn-success">TEST</button>
+        </div>
+        <div className="col-4">
+          <button className="btn btn-success">TEST</button>
+        </div>
+      </div>
+    </Base>
+  );
 }
