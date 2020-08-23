96d94168296dcf5c2852dd44a9e04bc2dba708ba
arunsnd2017 <arunsinghsnd@gmail.com>
2020-08-23 16:37:45 +0530

    creating compoents for stripe payment

--- src/core/StripeCheckout.js
+++ src/core/StripeCheckout.js
@@ -3,6 +3,8 @@ import { isAutheticated } from "../auth/helper";
 import { cartEmpty, loadCart } from "./helper/cartHelper";
 import { Link } from "react-router-dom";
 import StripeCheckoutButton from "react-stripe-checkout";
+import { API } from "../backend";
+import { createOrder } from "./helper/orderHelper";
 
 const StripeCheckout = ({
   products,
@@ -28,7 +30,23 @@ const StripeCheckout = ({
   };
 
   const makePayment = token => {
-    //
+    const body = {
+      token,
+      products
+    };
+    const headers = {
+      "Content-Type": "application/json"
+    };
+    return fetch(`${API}/stripepayment`, {
+      method: "POST",
+      headers,
+      body: JSON.stringify(body)
+    })
+      .then(response => {
+        console.log(response);
+        //call further methods
+      })
+      .catch(error => console.log(error));
   };
 
   const showStripeButton = () => {
@@ -38,8 +56,6 @@ const StripeCheckout = ({
         token={makePayment}
         amount={getFinalAmount() * 100}
         name="Buy Tshirts"
-        shippingAddress
-        billingAddress
       >
         <button className="btn btn-success">Pay with stripe</button>
       </StripeCheckoutButton>
